d284ab265bb711badee6d38ef19a0e21feb07eb6
Lazar Stojanović <lakisd98@gmail.com>
2023-05-04 08:22:55 +0200

    Add: feedback card markup and styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import classes from "./Card.module.css";
 
 const Card = (props) => {
-	return <section className={classes.card}>{props.children}</section>;
+	return <main className={`${classes.card} ${props.className}`}>{props.children}</main>;
 };
 
 export default Card;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,25 @@
 import Card from "../components/Card";
+import star from "../assets/images/icon-star.svg";
+import classes from "./HomePage.module.css";
 
 const HomePage = () => {
 	return (
-		<>
-			<Card />
-		</>
+		<Card>
+			<img src={star} alt="star image" className={classes.star} />
+			<h1>How did we do?</h1>
+			<p>
+				Please let us know how we did with your support request. All feedback is appreciated to help us improve our
+				offering!
+			</p>
+			<section>
+				<div>1</div>
+				<div>2</div>
+				<div>3</div>
+				<div>4</div>
+				<div>5</div>
+			</section>
+			<div className={classes["submit-button"]}>SUBMIT</div>
+		</Card>
 	);
 };
 
